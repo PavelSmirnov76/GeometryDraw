f8dc2d8956bafcc98123bbf1f210837e8da3e856
Павел Смирнов <cocacola76rus@gmail.com>
2022-08-17 11:51:51 +0300

    1. Add VM. 2. Add layout for task 1.

---
+++ GeometryDraw/Labs/Lab1/Task1.cs
@@ -0,0 +1,43 @@
+﻿namespace GeometryDraw.Labs.Lab1
+{
+    public class Point
+    {
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+    public class Line
+    {
+        public Line(double a, double b)
+        {
+            A = a;
+            B = b;
+        }
+        public double A { get; set; }
+        public double B { get; set; }
+    }
+    public class Task1<T> : ITask<T>
+    {
+        public Task1(Point pointA, Point pointB, Line line)
+        {
+            PointA = pointA;
+            PointB = pointB;
+            Line = line;
+            Answer = "";
+        }
+
+        public Point? PointA { get; set; }
+        public Point? PointB { get; set; }
+        public Line? Line { get; set; }
+
+        public string Answer { set; get; }
+
+        public void StartTask()
+        {
+        }
+    }
+}
--- GeometryDraw/MainWindow.xaml.cs
+++ GeometryDraw/MainWindow.xaml.cs
@@ -1,30 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using GeometryDraw.Labs.Lab1;
 
 namespace GeometryDraw
 {
-    
     public partial class MainWindow : Window
     {
         const double AxisXCenter = 200;
         const double AxisYCenter = 300;
 
-        
         public MainWindow()
         {
-            InitializeComponent();     
+            InitializeComponent();
+
+            Task1 = new Task1<int>(new Labs.Lab1.Point(0, 0), new Labs.Lab1.Point(0, 0), new Line(0, 0));
+
+            DataContext = Task1;
+        }
+
+        public Task1<int> Task1 { get; set; }
+
+        private void ButtonRunLab1Task1_Click(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
